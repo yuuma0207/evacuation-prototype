f3ce7a6d9377c440993a80249694ec556f02be7a
yuuma <yuuma0207@gmail.com>
2024-12-14 15:50:42 +0900

    Initial Commit

---
+++ evacuation-prototype/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  evacuation-prototype
+//
+//  Created by yuuma on 2024/12/14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+                
+                
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ evacuation-prototype/evacuation_prototypeApp.swift
@@ -0,0 +1,17 @@
+//
+//  evacuation_prototypeApp.swift
+//  evacuation-prototype
+//
+//  Created by yuuma on 2024/12/14.
+//
+
+import SwiftUI
+
+@main
+struct evacuation_prototypeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
