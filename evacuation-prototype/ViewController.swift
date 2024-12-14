cd13ec33c1076f6229ddfe2633d9539d44339c68
yuuma <yuuma0207@gmail.com>
2024-12-14 16:59:20 +0900

    replace: オレンジやリンゴを選択できるようにした

---
+++ evacuation-prototype/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewCpmtroller.swift
+//  evacuation-prototype
+//
+//  Created by yuuma on 2024/12/14.
+//
+
+import UIKit
+import MapKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var mapView: MKMapView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+}
